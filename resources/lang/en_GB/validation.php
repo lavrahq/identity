30f0ca5672d6274d1594df876c49855e9b84da03
Scott Plunkett <scottplunkett15@gmail.com>
2019-07-29 10:58:54 -0500

    New translations validation.php (English, United Kingdom)

--- resources/lang/en_GB/validation.php
+++ resources/lang/en_GB/validation.php
@@ -1,13 +1,8 @@
 <?php
 
 return [
-
     'pwned' => 'Your password is insufficiently secure as it has been found at least :min times in known password breaches, please choose a new one.',
-
     'min' => [
-
         'string' => 'This field must be at least :min characters.',
-
     ]
-
 ];
